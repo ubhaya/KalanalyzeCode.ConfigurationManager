f2c1eff1163f0f4b81cbb9751dbfb7ded32f26c6
Ubhaya Hettiarachchi <ubhaya123@gmail.com>
2024-08-01 13:22:01 +0530

    Update the db seeder to seed sample Projects

--- src/Manager/Application/Helpers/AppConstants.cs
+++ src/Manager/Application/Helpers/AppConstants.cs
@@ -8,7 +8,7 @@ public class AppConstants
     {
         public const string LoggingBehaviour = "Minimal API Request: {Name} {@Request}";
         public const string TransactionBehaviour = "Request failed: Rolling back all the changes made to the Context";
-        public const string DatabaseSeed = "Database seed. Count={Count}";
+        public const string DatabaseSeed = "Database seed to {Table}. Count={Count}";
         public const string DatabaseSaved = "Database saved successfully at {Date} - {Time}";
         public const string TransactionAlreadyCreated = "A transaction with ID {ID} is already created";
         public const string TransactionCreated = "A new transaction was created with ID {ID}";
